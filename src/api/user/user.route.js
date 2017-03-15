b53b4af50a0dfda6139bfe3eb1f0d543c015abb9
Ricardo <rferromoreno@gmail.com>
2017-03-15 15:10:33 -0300

    Deleting users via POST
    Agregada la posiblidiad de borrar usuarios mediante post {id:id_usuario}

--- src/api/user/user.route.js
+++ src/api/user/user.route.js
@@ -7,7 +7,6 @@ export default ({ connection }) => {
 
   // Create a new route with the prefix /
   let usersRoute = router.route('/');
-
   // Create endpoint / for GET
   usersRoute.get(function (req, res) {
       connection.query('SELECT id, usuario from usuarios', function(err, rows, fields) {
@@ -21,7 +20,6 @@ export default ({ connection }) => {
 
   // Create endpoint /:_id for GET
   usersRoute = router.route('/:_id');
-
   usersRoute.get(function (req, res) {
       // ojo con la INYECCION SQL
       let consulta = `SELECT id, usuario FROM usuarios WHERE id=${req.params._id}`;
@@ -37,7 +35,6 @@ export default ({ connection }) => {
 
     // Create endpoint /add {user:"pepito", pass:"12345"} for POST
   usersRoute = router.route('/add');
-
   usersRoute.post(function (req, res) {
       // ojo con la INYECCION SQL
       let consulta = `INSERT INTO usuarios (usuario, password) VALUES('${req.body.user}','${req.body.pass}');`;
@@ -50,6 +47,24 @@ export default ({ connection }) => {
         }
       });
   });
+
+    // Create endpoint /delete {id:123} for POST
+  usersRoute = router.route('/delete');
+  usersRoute.post(function (req, res) {
+      // ojo con la INYECCION SQL
+      let consulta = `DELETE FROM usuarios WHERE id=${req.body.id};`;
+      console.log(consulta);
+      connection.query(consulta, function(err, rows, fields) {
+        if (!err) {
+          res.json(rows);
+        } else {
+          res.send(err);
+        }
+      });
+  });
+
+
+
 
   return router;
 }
